4cd03a0541554e0c48919062983651abf4e15660
lazyswan <swanandcse@gmail.com>
2019-04-23 03:29:21 -0700

    Sensor Values Updates

--- app/src/main/java/com/example/hot_wheels/BluetoothChatService.java
+++ app/src/main/java/com/example/hot_wheels/BluetoothChatService.java
@@ -483,18 +483,24 @@ public class BluetoothChatService {
 
         public void run() {
             Log.i(TAG, "BEGIN mConnectedThread");
-            byte[] buffer = new byte[1024];
+            //byte[] buffer = new byte[1024];
             int bytes;
 
             // Keep listening to the InputStream while connected
             while (mState == STATE_CONNECTED) {
                 try {
                     // Read from the InputStream
-                    bytes = mmInStream.read(buffer);
+                    int bytesAvailable = mmInStream.available();
+                    if(bytesAvailable>0){
+                        byte[] buffer = new byte[bytesAvailable];
+                        bytes=mmInStream.read(buffer);
+                        mHandler.obtainMessage(Constants.MESSAGE_READ, bytes, -1, buffer)
+                                .sendToTarget();
+                    }
+                   // bytes = mmInStream.read(buffer);
 
                     // Send the obtained bytes to the UI Activity
-                    mHandler.obtainMessage(Constants.MESSAGE_READ, bytes, -1, buffer)
-                            .sendToTarget();
+
                 } catch (IOException e) {
                     Log.e(TAG, "disconnected", e);
                     connectionLost();
--- app/src/main/java/com/example/hot_wheels/Obstacle_Avoidance.java
+++ app/src/main/java/com/example/hot_wheels/Obstacle_Avoidance.java
@@ -1,3 +1,216 @@
+/*package com.example.hot_wheels;
+
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class Obstacle_Avoidance extends AppCompatActivity
+{
+
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+
+    private static final String START = "~1@!#";
+    private static final String STOP = "~0@!#";
+    private static final String TAG = "Communicate_Activity";
+    private static final String GEO = "GEO: ";
+    private static final String FS = "FS: ";
+    private static final String RS = "RS: ";
+    private static final String LS = "LS: ";
+    private static final String BS = "BS: ";
+    private String mConnectedDeviceAddress = null;
+
+    TextView mode_details_tv,fs_val,ls_val,rs_val,bs_val,geo_val;
+
+    public static String EXTRA_ADDRESS = "device_address";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obstacle__avoidance);
+        Intent intent = getIntent();
+        mConnectedDeviceAddress = intent.getStringExtra(MainActivity.EXTRA_ADDRESS);
+        //Bluetooth_Setting:---------------------------------
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Button startButton = (Button)findViewById(R.id.start_button);
+        Button stopButton = (Button)findViewById(R.id.stop_button);
+        Button exitButton = (Button)findViewById(R.id.exit_btn);
+        mode_details_tv = (TextView)findViewById(R.id.status_tv);
+        fs_val=findViewById(R.id.fs_tv);
+        ls_val=findViewById(R.id.ls_tv);
+        rs_val=findViewById(R.id.rs_tv);
+        bs_val=findViewById(R.id.bs_tv);
+        geo_val=findViewById(R.id.geo_tv);
+        //myTextbox = (EditText)findViewById(R.id.entry);
+        findBT();
+        try {
+            openBT();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Start Button
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                try
+                {
+                    sendData(START);
+                }
+                catch (IOException ex) { }
+            }
+        });
+
+        //Stop Button
+        stopButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                try
+                {
+                    sendData(STOP);
+                }
+                catch (IOException ex) { }
+            }
+        });
+
+        //Exit button
+        exitButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                try
+                {
+                    closeBT();
+                }
+                catch (IOException ex) { }
+                finish();
+            }
+        });
+    }
+
+    void findBT()
+    {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mmDevice = mBluetoothAdapter.getRemoteDevice(mConnectedDeviceAddress);
+
+    }
+
+    void openBT() throws IOException
+    {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID HC05
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        mmSocket.connect();
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
+
+        beginListenForData();
+
+        mode_details_tv.setText("Connected to HC05 ");
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        final byte delimiter = '#'; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            fs_val.setText(data.substring(data.indexOf('F'),data.indexOf('L')));
+                                            rs_val.setText(data.substring(data.indexOf('R')));
+                                            ls_val.setText(data.substring(data.indexOf('L'),data.indexOf('R')));
+                                            //mode_details_tv.setText(data);
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    void sendData(String msg) throws IOException
+    {
+
+        mmOutputStream.write(msg.getBytes());
+        mode_details_tv.setText("Data Sent");
+    }
+
+    void closeBT() throws IOException
+    {
+        stopWorker = true;
+        mmOutputStream.close();
+        mmInputStream.close();
+        mmSocket.close();
+        mode_details_tv.setText("Bluetooth Closed");
+    }
+}
+
+*/
+
+
 package com.example.hot_wheels;
 
 import android.bluetooth.BluetoothAdapter;
@@ -7,27 +220,48 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Obstacle_Avoidance extends AppCompatActivity {
     //Bluetooth_Setting:---------------------------------
     private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
     private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
     private static final int REQUEST_ENABLE_BT = 3;
+    private static final String START = "~1@!#";
+    private static final String STOP = "~0@!#";
     private static final String TAG = "Communicate_Activity";
+    private static final String GEO = "GEO: ";
+    private static final String FS = "FS: ";
+    private static final String RS = "RS: ";
+    private static final String LS = "LS: ";
+    private static final String BS = "BS: ";
+
     private BluetoothAdapter mBluetoothAdapter = null;
     private String mConnectedDeviceName = null;
     private String mConnectedDeviceAddress = null;
     private BluetoothChatService mChatService = null;
     private StringBuffer mOutStringBuffer;
     //Bluetooth_Setting:---------------------------------
-    byte[] readBuf;
+
+    //String sensor_vals[]={"0.0","0.0","0.0","0.0","0.0","0.0","0.0"};
+    //Recieving Data from Bridge
+    private static final char START_PACKET = '~';
+    private static final char END_PACKET   = '#';
+    ArrayList<String> sensor_vals_from_bridge;
+    String input_packet_string;
+    byte[] writeBuf;
+    boolean start_conversion=true;
+    //int rx_buff_ptr;
     Button start_btn,stop_btn;
-    TextView mode_details_tv;
+    TextView mode_details_tv,fs_val,ls_val,rs_val,bs_val,geo_val;
     //private String mConnectedDeviceAddress = null;
+    //ArrayList<String> recievedMsgList;
     public static String EXTRA_ADDRESS = "device_address";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +272,7 @@ public class Obstacle_Avoidance extends AppCompatActivity {
         //Bluetooth_Setting:---------------------------------
         mConnectedDeviceAddress = intent.getStringExtra(MainActivity.EXTRA_ADDRESS);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         //Bluetooth_Setting:---------------------------------
         init_listeners();
     }
@@ -47,8 +282,15 @@ public class Obstacle_Avoidance extends AppCompatActivity {
 
         start_btn=findViewById(R.id.start_button);
         stop_btn=findViewById(R.id.stop_button);
-        mode_details_tv=findViewById(R.id.tv_mode_details);
-        //rx_data=findViewById(R.id.rx_data);
+        mode_details_tv=findViewById(R.id.status_tv);
+        fs_val=findViewById(R.id.fs_tv);
+        ls_val=findViewById(R.id.ls_tv);
+        rs_val=findViewById(R.id.rs_tv);
+        bs_val=findViewById(R.id.bs_tv);
+        geo_val=findViewById(R.id.geo_tv);
+        //recievedMsgList=new ArrayList<>();
+        sensor_vals_from_bridge=new ArrayList<>();
+
     }
 
 //Bluetooth and Thread Communication Code-------------------------------------
@@ -115,15 +357,34 @@ public class Obstacle_Avoidance extends AppCompatActivity {
                     //rx_data.setText("Rx_Data: ");
                     break;
                 case Constants.MESSAGE_READ:
-                    readBuf = (byte[]) msg.obj;
-                    //readBuf.toString();
-                   String readMessage = new String(readBuf, 0, msg.arg1);
-                    //notify_user(" ",readMessage,false);
-                    //rx_data.setText("Rx_Data: "+readMessage);
-                    // construct a string from the valid bytes in the buffer
+                    byte[] readBuf = (byte[]) msg.obj;
 
-                    //notify_user(" ","Incoming Message Ignored",false);
-                    // mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    Log.i("MSG FROM AQUIB***: ",readMessage+" "+readBuf.length);
+                    for(int i=0;i<readBuf.length;i++){
+                        byte rx_char=readBuf[i];
+                        if(rx_char == START_PACKET){
+                            input_packet_string="";
+                            input_packet_string=Character.toString((char)rx_char);
+                        }
+                        else if(rx_char == END_PACKET){
+                            input_packet_string+=Character.toString((char)rx_char);
+                            parse_msg(input_packet_string);
+                            input_packet_string="";
+
+                        }
+                        else{
+                            input_packet_string+=Character.toString((char)rx_char);
+                        }
+                    }
+                    /*if(readMessage.contains("~") && readMessage.contains("#") ){
+                        copy_of_input_data=readMessage.substring(readMessage.indexOf('~'),readMessage.indexOf('#')+1);
+                        parse_msg(copy_of_input_data);
+                    }*/
+
+
+
+
                     break;
                 case Constants.MESSAGE_DEVICE_NAME:
                     // save the connected device's name
@@ -136,6 +397,24 @@ public class Obstacle_Avoidance extends AppCompatActivity {
             return false;
         }
     });
+
+    private void parse_msg(String readMessage) {
+        Log.i("parse_msg : ","PARSED_PACKET: "+readMessage);
+
+
+
+            ls_val.setText("LS: "+readMessage.substring(readMessage.indexOf('L')+1,readMessage.indexOf('R')));
+            fs_val.setText("FS: "+readMessage.substring(readMessage.indexOf('F')+1,readMessage.indexOf('B')));
+            rs_val.setText("RS: "+readMessage.substring(readMessage.indexOf('R')+1,readMessage.indexOf('F')));
+            bs_val.setText("BS: "+readMessage.substring(readMessage.indexOf('B')+1,readMessage.indexOf('@')));
+        geo_val.setText("Lat: "+readMessage.substring(readMessage.indexOf('@')+1,readMessage.indexOf('!'))+
+                " Long: "+readMessage.substring(readMessage.indexOf('!')+1,readMessage.indexOf('#')));
+        //bs_val.setText("BS: "+readMessage.substring(readMessage.indexOf('B'+1),readMessage.indexOf('#')));
+
+
+
+
+    }
 
 
     private void sendMessage(String message) {
@@ -159,13 +438,14 @@ public class Obstacle_Avoidance extends AppCompatActivity {
 //Bluetooth and Thread Communication Code-------------------------------------
 
     public void start_btn_click(View view) {
-        String start_cmd ="~1@!#";
-        sendMessage(start_cmd);//send 1
+
+        sendMessage(START);//send 1
     }
 
     public void stop_btn_click(View view) {
-        String stop_cmd = "~0@!#";
-        sendMessage(stop_cmd);//send 0
+        //String stop_cmd = STOP;
+        sendMessage(STOP);//send 0
+        Log.i("SEND: ", "BUTTON PRESS");
     }
 
     public void exit_btn_click(View view) {
@@ -174,6 +454,7 @@ public class Obstacle_Avoidance extends AppCompatActivity {
         }
         mBluetoothAdapter.cancelDiscovery();
         //  mBluetoothAdapter.disable();//TurnOFF Bluetooth
+        //recievedMsgList.clear();
         finish();
     }
 
@@ -181,13 +462,10 @@ public class Obstacle_Avoidance extends AppCompatActivity {
         if (toast) {
             Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
         }
-        mode_details_tv.setText(prefix + message);
+        //mode_details_tv.setText(prefix + message);
 
     }
 
-    public void log_btn(View view) {
-        Intent newintent = new Intent(this, Log_screen.class);
-        newintent.putExtra(EXTRA_ADDRESS,mConnectedDeviceAddress);
-        startActivity(newintent);
-    }
+
 }
+
